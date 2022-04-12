985f5ddc8db30eea24d3c085b418e0221a057433
wearethewinds <fp@skillers.com>
2022-04-12 13:34:52 +0200

    Fix deprecation of TreeBuilder::root
    with backwards compatibility

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('hslavich_simplesamlphp');
+        $treeBuilder = new TreeBuilder('hslavich_simplesamlphp');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('hslavich_simplesamlphp');
+        }
 
         $rootNode
             ->children()
@@ -26,7 +31,5 @@ class Configuration implements ConfigurationInterface
                 ->scalarNode('authentication_attribute')->defaultValue('uid')->end()
             ->end()
         ;
-
-        return $treeBuilder;
     }
 }
